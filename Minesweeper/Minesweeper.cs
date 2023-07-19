7812051faba0c9d8ff9cae31e20cf2c8d62e8f81
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-19 10:01:54 +0200

    Create the game board

--- Minesweeper/Minesweeper.cs
+++ Minesweeper/Minesweeper.cs
@@ -8,4 +8,19 @@ class Tile
     public bool isOpen;
     public int numberOfBombsAround;
 }
+class Minesweeper
+{
 
+    static Tile[,] createGameBoard()
+    {
+        Tile[,] gameBoard = new Tile[5, 5];
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                gameBoard[x, y] = new Tile { x = x, y = y, hasBomb = false, isOpen = false, numberOfBombsAround = 0 };
+            }
+        }
+        return gameBoard;
+    }
+}
